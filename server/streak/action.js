8efafb3937ae9cb996443b3a174b98548d910c09
mikemajesty <mikemajesty2011@gmail.com>
2017-10-07 18:03:51 -0300

    chnage action streak

--- server/streak/action.js
+++ server/streak/action.js
@@ -2,7 +2,7 @@ const streakService = require("./service");
 
 const getScrapStreak = (req, res, next) => {
   streakService.getStreakBody(req.body.username).then(function(params) {
-    res.send(JSON.stringify(params));
+    res.send(params);
   });
 };
 
