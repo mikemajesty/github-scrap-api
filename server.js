da836ff2bccb5d78f9689b8780da1f450b38bad5
mikemajesty <mikemajesty2011@gmail.com>
2017-10-06 21:11:22 -0300

    add server js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+	console.log(`API listening on port ${port}`);
+});
+
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+app.use('/', require('server/api'));
