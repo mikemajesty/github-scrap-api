3c4835b81f4659ad0398fa652233d34a6c88cfa3
mikemajesty <mikemajesty2011@gmail.com>
2017-10-06 23:36:54 -0300

    change project name

--- server/repository/router.js
+++ server/repository/router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const action = require('./action');
 
 router.post('/full', action.getRepository);
-router.post('/rules', action.getRulesRepository);
+router.post('/format', action.getRulesRepository);
 router.post('/stars', action.getStars);
 router.post('/forks', action.getForks);
 router.post('/best', action.getBestRepository);
