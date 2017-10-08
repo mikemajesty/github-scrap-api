28de389a6ab770b3a9ff69c1878ed0d1c290ffca
mikemajesty <mikemajesty2011@gmail.com>
2017-10-08 20:51:39 -0300

    add index page

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+const favicon = require('serve-favicon');
 
 app.use(bodyParser.json());
 
+app.use(favicon('static/img/favicon.ico'));
+
 app.listen(port, () => {
 	console.log(`API listening on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.sendfile('static/index.html');
 });
 
 app.use((req, res, next) => {
