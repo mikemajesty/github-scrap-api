e42f3119b768b74fc5dc3b369a26e8a8fcc57090
mikemajesty <mikemajesty2011@gmail.com>
2017-11-19 19:29:58 -0200

    add cors

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const favicon = require('serve-favicon');
-const fs = require('fs');
+const cors = require('cors');
+ 
+app.use(cors())
 
 app.use(bodyParser.json());
 
